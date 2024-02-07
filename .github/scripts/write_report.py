e9c1c3806bed1894443da682fb100beef2ea1cf9
Dan Sandiford <sonderfjord@gmail.com>
2024-02-07 13:14:11 +1100

    Update write_report.py

--- .github/scripts/write_report.py
+++ .github/scripts/write_report.py
@@ -14,7 +14,7 @@ repo = g.get_repo("ModelAtlasofTheEarth/Model_Submission")
 issue = repo.get_issue(number = issue_number)
 
 # Parse issue
-data, error_log = parse_issue(issue)
+data, error_log = parse_issue(issue)`request review label`
 
 # Write report
 report = "Thank you for submitting. Please check the output below, and fix any errors, etc.\n\n"
@@ -24,7 +24,7 @@ report = """Thank you for submitting. \n
 * Please check the report below, particularly the warning and errors section, and website materials \n 
 * You can fix any errors, or update fields, by editing the markdown file at the top of the issue \n 
 * Edits will trigger the report will be regenerated \n 
-* once you are satisfied with the results, please add an `request review label` \n 
+* once you are satisfied with the results, please add an https://github.com/ModelAtlasofTheEarth/Model_Submission/labels/review%20requested \n 
 * once the `model_reviewers` team has approved the model, we will create a repository for your model \n\n""" 
 
 report += "# Errors and Warnings \n"
