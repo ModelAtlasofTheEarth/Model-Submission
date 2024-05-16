d904930d4d56c26987e154c59e5ef5e0690695ff
dansand <sonderfjord@gmail.com>
2024-05-16 15:08:37 +1000

    copy_files needed safeguarding against empy string

--- .github/scripts/copy_files.py
+++ .github/scripts/copy_files.py
@@ -1,9 +1,25 @@
 import requests
 
-def copy_files(repo, directory, issue_dict):
-	file_keys = ["landing_image", "animation", "graphic_abstract", "model_setup_figure"]
+#def copy_files(repo, directory, issue_dict):
+#	file_keys = ["landing_image", "animation", "graphic_abstract", "model_setup_figure"]
 
-	for file_key in file_keys:
-		if file_key in issue_dict:
-			response = requests.get(issue_dict[file_key]["url"])
-			repo.create_file(directory+issue_dict[file_key]["filename"], "add "+issue_dict[file_key]["filename"], response.content)
+#	for file_key in file_keys:
+#		if file_key in issue_dict:
+#			response = requests.get(issue_dict[file_key]["url"])
+#			repo.create_file(directory+issue_dict[file_key]["filename"], "add "+issue_dict[file_key]["filename"], response.content)
+
+
+def copy_files(repo, directory, issue_dict):
+    file_keys = ["landing_image", "animation", "graphic_abstract", "model_setup_figure"]
+
+    for file_key in file_keys:
+        if file_key in issue_dict:
+            file_info = issue_dict[file_key]
+            url = file_info.get("url", "")
+
+            # Skip if the URL is an empty string
+            if url:
+                response = requests.get(url)
+                repo.create_file(directory + file_info["filename"], "add " + file_info["filename"], response.content)
+            else:
+                print(f"Skipping {file_key} as the URL is empty")
